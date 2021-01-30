7393102b6f8db3f6476b3729814071789e6ba4fd
Sajiv Graham <saj-2000@hotmail.com>
2021-01-29 21:19:23 -0500

    second commit

--- LAB1.py
+++ LAB1.py
@@ -1,2 +1,17 @@
 def hello():
     print("ECSE3038 - Engineering IoT Systems")
+
+def validatePassword(password):
+    x=password
+    y= len(x)
+    z=0
+    if y >= 8 and x.isalnum():
+        for val in x:
+            try:
+                if isinstance(int(val), int):
+                    z = z+1
+            except:
+                pass
+        if z >=2 :
+            return True
+    return False
